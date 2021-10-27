43305188c5cbe676a280e18cbd0da333eff26a47
oikawa-takeshi <oikawa-t@jobcrown.co.jp>
2021-10-27 22:32:23 +0900

    v2.0.0 テーブル拡張

--- SQL/new add.sql
+++ SQL/new add.sql
@@ -11,3 +11,17 @@ CREATE TABLE equipment (
 
 ALTER TABLE  userdata ADD lv INT DEFAULT 1 AFTER email;
 ALTER TABLE  userdata ADD exp INT DEFAULT 0 AFTER Lv;
+
+
+create table statistics_user_gacha (
+  id int auto_increment primary key,
+  acc varchar(20) not null,
+  gacha int not null,
+  gachaGroup int not null
+);
+
+truncate table statistics_user_gacha;
+alter table statistics_user drop column gacha;
+alter table statistics_user add column maxArrivedFloor int default 0;
+alter table statistics_user add column lastFloorCount int default 0;
+
