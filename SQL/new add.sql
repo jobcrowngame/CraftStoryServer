05bdb0e3f50a1399e41ea9f701991a57d0c25ef2
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-11-12 17:35:12 +0900

    add new coloum

--- SQL/new add.sql
+++ SQL/new add.sql
@@ -5,3 +5,5 @@ alter table statistics_user add column totalSetBlockCount int default 0;
 alter table limited add column guide_end3 int default 0 after guide_end2;
 alter table limited add column guide_end4 int default 0 after guide_end3;
 
+alter table limited add column main_task int default 1;
+alter table limited add column main_task_count int default 0;
