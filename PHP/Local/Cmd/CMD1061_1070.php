e2a8242c5f8cc72f0b159876985b2c0a1db6c751
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-11-19 13:29:50 +0900

    1061　maxArrivedFloorがnullの場合デフォルトで0にする

--- PHP/Local/Cmd/CMD1061_1070.php
+++ PHP/Local/Cmd/CMD1061_1070.php
@@ -7,6 +7,10 @@ class CMD1061_1070{
         $sql = "SELECT maxArrivedFloor FROM statistics_user WHERE acc = '$acc'";
         $result = MySqlPDB::$pdo->query($sql)->fetch();
 
+        if (empty($result['maxArrivedFloor'])){
+            $result['maxArrivedFloor'] = 0;
+        }
+        
         Common::Send(array(
             'maxArrivedFloor'=>$result['maxArrivedFloor'],
         ));
