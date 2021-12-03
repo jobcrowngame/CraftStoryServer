2e972b3c3df8abbaad7cc7f993017a81806bd6ce
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-12-03 14:03:28 +0900

    add coloum totalPoint to userdata table

--- SQL/new add.sql
+++ SQL/new add.sql
@@ -1,16 +1,5 @@
 SET SQL_SAFE_UPDATES = 0;
 use mydb;
 
-alter table notice add column priority int after activedate;
-alter table notice add column pickup int after priority;
-
-alter table loginbonus add column type int after active;
-alter table loginbonus add column items Text after type;
-alter table loginbonus add column itemCounts Text after items;
-alter table loginbonus add column themeTexture Text after type;
-alter table loginbonus drop column loginbonusid;
-
-alter table limited drop column loginbonus_added;
-alter table limited add column loginBonus Text after logined;
-alter table limited add column loginBonusStep Text after loginBonus;
+alter table userdata add column totalPoint int default 0 after coin3;
 
