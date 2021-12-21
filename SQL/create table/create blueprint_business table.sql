c040e1d390814c38f26e386ffec79858f236752e
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-12-21 21:51:14 +0900

    テーブル作成更新

--- SQL/create table/create blueprint_business table.sql
+++ SQL/create table/create blueprint_business table.sql
@@ -1,10 +1,10 @@
-use mydb;
-
+use test;
+drop table blueprint_business;
 CREATE TABLE blueprint_business (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     from_user VARCHAR(30) NOT NULL,
     to_user VARCHAR(30) NOT NULL,
     blueprint_name VARCHAR(30) NOT NULL,
-    price int  NOT NULL
+    price int  NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
