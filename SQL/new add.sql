9ac0f782c27bd6c80b100571e55d06f3aa1ce96c
jobcrowngame1 <jobcrown.game.1@gmail.com>
2021-12-22 10:14:29 +0900

    新規テーブル追加

--- SQL/new add.sql
+++ SQL/new add.sql
@@ -2,3 +2,12 @@ SET SQL_SAFE_UPDATES = 0;
 use mydb;
 
 alter table notice add column active bool default 1 after id;
+
+CREATE TABLE blueprint_business (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    from_user VARCHAR(30) NOT NULL,
+    to_user VARCHAR(30) NOT NULL,
+    blueprint_name VARCHAR(30) NOT NULL,
+    price int  NOT NULL
+);
